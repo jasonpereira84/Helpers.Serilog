4cf81881568fe6787abab29f03a015b5f7ab4f4b
Jason Pereira <jasonpereira84@gmail.com>
2026-01-25 02:35:33 -0600

    Added SensitiveDataDestructuringPolicy.cs

---
+++ Helpers.Serilog/SensitiveDataDestructuringPolicy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace JasonPereira84.Helpers
+{
+    namespace Serilog
+    {
+        public class SensitiveDataDestructuringPolicy : IDestructuringPolicy
+        {
+            public Boolean TryDestructure(Object value, ILogEventPropertyValueFactory factory, out LogEventPropertyValue? result)
+            {
+                // Avoid infinite recursion by checking if we are already destructuring
+                var type = value.GetType();
+                var props = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+                // Create a list of log properties, masking those with the attribute
+                var logProperties = props.Select(p => {
+                    var val = p.GetValue(value);
+                    if (p.GetCustomAttribute<SensitiveDataAttribute>() != null)
+                        return new LogEventProperty(p.Name, new ScalarValue("***MASKED***"));
+
+                    return new LogEventProperty(p.Name, factory.CreatePropertyValue(val, true));
+                });
+
+                result = new StructureValue(logProperties, type.Name);
+                return true;
+            }
+        }
+    }
+}
